1373a5e2133c76adf96dda05a9a2e8f13af8aff7
gingersmith4 <88351672+gingersmith4@users.noreply.github.com>
2024-05-06 14:19:37 -0700

    changing volume coauthored by Riana

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,6 +1,7 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+let audioChoice = "";
 
 function init() {
   // TODO
@@ -10,18 +11,21 @@ function init() {
   The correct audio sound file should be set 
   */
   const horn = document.getElementById("horn-select");
-  horn.addEventListener("input", updateValue);
+  horn.addEventListener("input", updateHorn);
 
-  function updateValue() {
+  function updateHorn() {
     let choice = horn.value;
     console.log(choice);
     let choiceImage = document.querySelector('img');
-    let path = "assets/images/" + choice + ".svg";
-    choiceImage.src = path;
+    let imgPath = "assets/images/" + choice + ".svg";
+    choiceImage.src = imgPath;
+    let audioPath = "assets/audio/" + choice + ".mp3";
+    audioChoice = audioPath;
   }
 
 
-  /* When you change the volume on the slider, the following should occur:
+  /* // TODO
+  When you change the volume on the slider, the following should occur:
     At zero volume, the mute icon (level 0) should be displayed
     From 1 to < 33 volume the first volume level should be displayed
     From 33 to < 67 volume the second volume level should be displayed
@@ -30,8 +34,25 @@ function init() {
     The corresponding volume should be set for the audio element 
     (note: this element’s volume is not out of 100, it's out of 1)
   */
+  let audio = document.getElementById("volume");
+  audio.addEventListener("input", updateAudio);
+  function updateAudio() {
+    let currVolume = audio.value;
+    let soundImg = document.getElementById("volume-controls").querySelector('img');
+    if(currVolume == 0) {
+      soundImg.src = "assets/icons/volume-level-0.svg";
+    } else if(currVolume < 33) {
+      soundImg.src = "assets/icons/volume-level-1.svg";
+    } else if (currVolume < 67) {
+      soundImg.src = "assets/icons/volume-level-2.svg";
+    } else {
+      soundImg.src = "assets/icons/volume-level-3.svg";
+    }
+    audio.volume = currVolume / 100;
+  }
 
-  /*
+
+  /* // TODO 
   When you click the “Play Sound” button the following should occur:
     The corresponding sound for the horn selected should play out loud at the specified volume
     If the Party Horn is selected, confetti should shoot out out, as shown in the video
