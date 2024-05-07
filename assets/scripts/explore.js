5e1a901bdcdba7545c93032334d136ea602edcf5
gingersmith4 <88351672+gingersmith4@users.noreply.github.com>
2024-05-07 16:06:35 -0700

    Face image changes when speech is being synthesized

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,16 +3,12 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
   /*  
   On page load, all of the available voices that you can use for your SpeechSynthesizer 
   should be loaded and populate the “Select Voice” dropdown. 
   (These are browser specific, so you might get different ones browser to browser).
-    */
-
-  /* We referenced code from this site: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
   */
- 
+  // We referenced code from this site: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
   const synth = window.speechSynthesis;
   let menu = document.getElementById("voice-select");
 
@@ -28,31 +24,34 @@ function init() {
 
       menu.appendChild(option);
     }
-});
+  });
 
-    //TODO
-    /*
-    When you click the “Press to Talk” button, the following should happen:
-The text that you have typed into the “Text to speak here” 
-textarea should be spoken out loud using the voice that you have selected
-    */
-//We referenced this code: ://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
-let talkButton = document.querySelector('button');
-talkButton.addEventListener("click", playAudio);
-function playAudio() {
-  var text = document.getElementById("text-to-speak").value;  
-  console.log(text);
-  const sayThis = new SpeechSynthesisUtterance(text);
-  synth.voice = menu.value;
-  console.log(synth.voice);
-synth.speak(sayThis);
+  //TODO
+  /*
+  When you click the “Press to Talk” button, the following should happen:
+  The text that you have typed into the “Text to speak here” 
+  textarea should be spoken out loud using the voice that you have selected
+  
+  Only while the synthesizer is speaking, the face
+  should swap to being open mouthed (included in the images folder)
+  */   
+  //We referenced this code: ://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
+  let faceImage = document.querySelector('img');
+  let talkButton = document.querySelector('button');
+  talkButton.addEventListener("click", playAudio);
 
-}
-
-//TODO 
-/*
-Only while the synthesizer is speaking, the face
- should swap to being open mouthed (included in the images folder)
-*/
+  function playAudio() {
+    var text = document.getElementById("text-to-speak").value;  
+    const sayThis = new SpeechSynthesisUtterance(text);
+    synth.voice = menu.value;
+    console.log(synth.voice);
+    synth.speak(sayThis);
+    sayThis.addEventListener("start", (event) => {
+      faceImage.src = "assets/images/smiling-open.png";
+    });
+    sayThis.addEventListener("end", (event) => {
+      faceImage.src = "assets/images/smiling.png";
+    });
+  }
 
 }
